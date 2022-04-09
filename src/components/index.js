8ae384c6697f47ad526c6005928b89c502dd101f
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-09 17:22:49 +0530

    feat: define routes for Trash,Archieve,label

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,7 @@ import Landing from "./Landing/Landing";
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Home from "./Home/Home";
-export { Navbar, Landing, Login, Signup, Home };
+import Archieve from "./Archieve/Archieve";
+import Trash from "./Trash/Trash";
+import Label from "./Label/Label";
+export { Navbar, Landing, Login, Signup, Home, Archieve, Trash, Label };
