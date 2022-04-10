d32886d6dcf7c708e9804e5167c12c4e3167c171
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-10 14:29:41 +0530

    feat: save title, description, pin in state

--- src/Context/notes-context.js
+++ src/Context/notes-context.js
@@ -5,18 +5,29 @@ const NoteContext = createContext();
 const useNote = () => useContext(NoteContext);
 
 const noteReducer = (noteState, action) => {
+  console.log(action);
   switch (action.type) {
     case "SET_TITLE":
-      break;
+      return { ...noteState, title: action.payload };
     case "SET_DESCRIPTION":
-
+      return { ...noteState, description: action.payload };
     case "SET_PIN":
-
+      return { ...noteState, isPin: action.payload };
     case "SET_CARD_COLOR":
 
     case "SET_LABEL":
 
     case "SET_PRIORITY":
+
+    case "CLEAR_DATA":
+      return {
+        title: "",
+        description: "",
+        isPin: false,
+        addLabel: "",
+        cardColor: "",
+        priority: "",
+      };
     default:
       return { ...noteState };
   }
