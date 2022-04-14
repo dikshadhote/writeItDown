69710c85eeabecd0982271a79ca8b18e5dfaf18e
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-14 22:29:45 +0530

    feat: decrease value of timer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function Login() {
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loggedIn = (login) => {
+    console.log(login);
+  };
+
   return (
     <div>
       <div className="d-flex flex-justify-center align-items-center mb-2">
@@ -8,51 +18,72 @@ export default function Login() {
           <div className="d-flex flex-justify-center">
             <h3 className="white-text-color">Login</h3>
           </div>
-          <div className=" mt-2 ml-1 d-flex flex-column">
-            <label htmlFor="input-email" className="ml-2  white-text-color">
-              Email
-            </label>
-            <input
-              className="input input-login black-light-bg white-text-color "
-              id="input-email"
-              placeholder="you@dreamstore.com"
-            />
-          </div>
-          <div className=" mt-2 ml-1 d-flex flex-column">
-            <label htmlFor="input-password" className="ml-2 white-text-color ">
-              Password
-            </label>
-            <input
-              className="input input-login black-light-bg white-text-color"
-              id="input-password"
-              placeholder="*******"
-            />
-          </div>
-
-          <div className="d-flex flex-justify-around align-items-center">
-            <span className="d-flex align-items-center">
-              <input
-                id="checkbox-unchecked"
-                className="form-check-input"
-                type="checkbox"
-              />
-              <label
-                htmlFor="checkbox-unchecked"
-                className="fs-small white-text-color"
-              >
-                Remember me?
-              </label>
-            </span>
-            <a href="#" className="black-text-color fs-small mr-2">
-              Forgot your password?
-            </a>
-          </div>
-          <button
-            type="button"
-            className="btn orange-bg login-button ml-3 font-weight-bold"
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              loggedIn(login);
+            }}
           >
-            Login
-          </button>
+            <div className=" mt-2 ml-1 d-flex flex-column">
+              <label htmlFor="input-email" className="ml-2  white-text-color">
+                Email
+              </label>
+              <input
+                className="input input-login black-light-bg white-text-color "
+                id="input-email"
+                placeholder="you@dreamstore.com"
+                type="email"
+                onChange={(e) => {
+                  let timer = setTimeout(() => {
+                    setLogin({ ...login, email: e.target.value });
+                    clearTimeout(timer);
+                  }, 1000);
+                }}
+              />
+            </div>
+            <div className=" mt-2 ml-1 d-flex flex-column">
+              <label htmlFor="input-password" className="ml-2 white-text-color">
+                Password
+              </label>
+              <input
+                className="input input-login black-light-bg white-text-color"
+                id="input-password"
+                placeholder="*******"
+                type="password"
+                onChange={(e) => {
+                  console.log("sjhjsh");
+                  let timer = setTimeout(() => {
+                    setLogin({ ...login, password: "hello" });
+                    clearTimeout(timer);
+                  }, 1000);
+                }}
+              />
+            </div>
+            <div className="d-flex flex-justify-around align-items-center">
+              <span className="d-flex align-items-center">
+                <input
+                  id="checkbox-unchecked"
+                  className="form-check-input"
+                  type="checkbox"
+                />
+                <label
+                  htmlFor="checkbox-unchecked"
+                  className="fs-small white-text-color"
+                >
+                  Remember me?
+                </label>
+              </span>
+              <a href="#" className="black-text-color fs-small mr-2">
+                Forgot your password?
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="btn orange-bg login-button ml-3 font-weight-bold"
+            >
+              Login
+            </button>
+          </form>
           <div className="d-flex flex-justify-center align-items-center">
             <Link
               className="white-text-color d-flex flex-justify-center align-items-center"
