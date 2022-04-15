f58e3b2d6f90f7098370301c1be0dfbeb22f0417
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-15 23:51:55 +0530

    refactor:create note component

---
+++ src/components/colorsDB.js
@@ -0,0 +1,33 @@
+let colors = [
+  { inputColor: "black-light-bg", cardColor: "black-bg" },
+  {
+    inputColor: "light-brown-bg",
+    cardColor: "brown-bg",
+  },
+  {
+    inputColor: "olive-light-bg",
+    cardColor: "olive-dark-bg",
+  },
+  {
+    inputColor: "violet-light-bg ",
+    cardColor: "violet-bg",
+  },
+  {
+    inputColor: "yellow-green-light",
+    cardColor: "yellow-green",
+  },
+  {
+    inputColor: "muddy-blue-light ",
+    cardColor: "muddy-blue",
+  },
+  {
+    inputColor: "green-light ",
+    cardColor: "green-bg",
+  },
+  {
+    inputColor: "greenish-blue-light",
+    cardColor: "greenish-blue-bg ",
+  },
+];
+
+export { colors };
