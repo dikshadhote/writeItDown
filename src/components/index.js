cd74ee2e9a292ab862dc54945deb886ef3017125
Diksha Dhote <dikshadhote7042@gmail.com>
2022-04-16 22:52:01 +0530

    chore: arrange import alphabetically

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,26 @@
-import Navbar from "./Navbar/Navbar";
+import Archieve from "./Archieve/Archieve";
+import CreateNote from "./Home/CreateNote";
+import DisplayNote from "./Home/DisplayNote";
+import Home from "./Home/Home";
+import Label from "./Label/Label";
 import Landing from "./Landing/Landing";
 import Login from "./Login/Login";
-import Signup from "./Signup/Signup";
-import Home from "./Home/Home";
-import Archieve from "./Archieve/Archieve";
-import Trash from "./Trash/Trash";
-import Label from "./Label/Label";
 import MockmanComponent from "./Mockman/Mockman";
+import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Sidebar/Sidebar";
+import Signup from "./Signup/Signup";
+import Trash from "./Trash/Trash";
 export {
-  Navbar,
+  Archieve,
+  CreateNote,
+  DisplayNote,
+  Home,
+  Label,
   Landing,
   Login,
-  Signup,
-  Home,
-  Archieve,
-  Trash,
-  Label,
   MockmanComponent,
+  Navbar,
+  Sidebar,
+  Signup,
+  Trash,
 };
